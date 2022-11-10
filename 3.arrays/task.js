a71b10a6f83839fe68e6782e7bd4b0183d989d97
Aznavyr <cesar-88@mail.ru>
2022-11-10 20:00:38 +0300

    AddedTaskArray

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = arr1.length === arr2.length && arr1.every(function(element, index) {
+    return element === arr2[index]; 
+});
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
+  return resultArr;
 }
